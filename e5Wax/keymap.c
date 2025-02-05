b4b1bb764613b6f323625ee95b402f7ca2945d96
Christian Klauser <christian.klauser@digitecgalaxus.ch>
2025-02-05 18:30:43 +0100

    Fix Meh

--- e5Wax/keymap.c
+++ e5Wax/keymap.c
@@ -49,14 +49,15 @@ enum custom_keycodes {
   } \
   break; }
 
+#define MOD_BIT_MEH MOD_BIT(KC_LEFT_SHIFT) | MOD_BIT(KC_LEFT_ALT) | MOD_BIT(KC_LEFT_CTRL)
 void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
   switch(keycode){
     SMTD_MT(CKC_A, KC_A, KC_LEFT_GUI, 1)
     SMTD_MT(CKC_S, KC_S, KC_LEFT_ALT, 1)
     SMTD_MT(CKC_D, KC_D, KC_LEFT_CTRL, 1)
     SMTD_MT(CKC_F, KC_F, KC_LEFT_SHIFT, 1)
-    SMTD_MT(CKC_G, KC_G, KC_MEH, 1)
-    SMTD_MT(CKC_H, KC_H, KC_MEH, 1)
+    SMTD_MT(CKC_G, KC_G, MOD_BIT_MEH, 1)
+    SMTD_MT(CKC_H, KC_H, MOD_BIT_MEH, 1)
     SMTD_MT(CKC_J, KC_J, KC_RIGHT_SHIFT, 1)
     SMTD_MT(CKC_K, KC_K, KC_RIGHT_CTRL, 1)
     SMTD_MT(CKC_L, KC_L, KC_LEFT_ALT, 1)
